976d39b74e99b583fc516659cbd18d5b00ff88b2
Alexander Zavyalov <whmordred@gmail.com>
2018-05-17 09:49:21 +0800

    Система аутентификации Laravel - только вход и выход

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/home', 'HomeController@index')->name('home');
